5ea5d752d6c5e2d321248c29a2563491592b237c
Rafaelmedeiros29 <139690138+RafaelMedeiros28@users.noreply.github.com>
2024-04-16 21:39:05 -0300

    data da cotação

--- Testes de Python/conversor de moeda simples.py
+++ Testes de Python/conversor de moeda simples.py
@@ -1,7 +1,9 @@
 valor = float(input("Dígite o valor em Reais: R$"))
 
-dolar = valor / 5.29
-euro = valor / 5.62
+dolar = valor / 5.29 #Cotação de 16/04/2024
+euro = valor / 5.62 #Cotação de 16/04/2024
 
 print(f'R${valor:.2f} em Dólar é igual a US${dolar:.2f}')
 print(f'R${valor:.2f} em Euros é igual a €{euro:.2f}')
+
+#Feito por Rafael Medeiros
